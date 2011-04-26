4202bd3565802ee9b1325d56526ca43f53469cec
Dan Lynch <dlynch@skatepark.local>
2011-04-26 10:02:36 -0700

    moved CDPage

---
+++ eBook/CDPage.h
@@ -0,0 +1,18 @@
+//
+//  CDPage.h
+//  eBook
+//
+//  Created by Dan Lynch on 4/26/11.
+//  Copyright (c) 2011 Pyramation Media. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface CDPage : NSManagedObject {
+@private
+}
+@property (nonatomic, retain) id page;
+
+@end
